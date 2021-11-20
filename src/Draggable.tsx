2f3a4222f3638261aef5ad8fe4e28c293ba54ac2
Jang Haemin <io@jhaemin.com>
2021-11-20 21:56:16 +0900

    Initialize the project with tsc

---
+++ src/Draggable.tsx
@@ -0,0 +1,169 @@
+import React, { useCallback, useEffect, useRef } from 'react'
+
+export const isTouchScreen =
+  typeof window !== 'undefined' &&
+  window.matchMedia('(pointer: coarse)').matches
+
+const { map, only } = React.Children
+
+const getScreenDistance = (
+  e: MouseEvent | TouchEvent,
+  direction: 'screenX' | 'screenY'
+) => {
+  if (isTouchScreen) {
+    return (e as TouchEvent).touches[0][direction]
+  }
+
+  return (e as MouseEvent)[direction]
+}
+
+export type DraggableData = {
+  /**
+   * Updated x-axis position based on the given `basePosition`.
+   * If you don't pass anything to `basePosition`, it is same as `deltaX`.
+   */
+  x: number
+  /**
+   * Updated y-axis position based on the given base position.
+   * If you don't pass anything to `basePosition`, it is same as `deltaY`.
+   */
+  y: number
+  /**
+   * x-axis Distance from the drag start.
+   */
+  deltaX: number
+  /**
+   * y-axis Distance from the drag start.
+   */
+  deltaY: number
+}
+
+export type DraggableCallback = (data: DraggableData) => void
+
+export type DraggableProps = {
+  stopPropagation?: boolean
+  onDrag?: DraggableCallback
+  onStart?: DraggableCallback
+  onEnd?: DraggableCallback
+  /**
+   * Used as an initial x, y when drag start.
+   * Even if you pass a state value and change it,
+   * never affects until the next drag start.
+   */
+  basePosition?: { x: number; y: number }
+}
+
+const Draggable: React.FC<DraggableProps> = ({
+  children,
+  stopPropagation,
+  onDrag,
+  onStart,
+  onEnd,
+  basePosition = { x: 0, y: 0 },
+}) => {
+  const childRef = useRef<HTMLElement>(null!)
+  const dragInfoRef = useRef({
+    eventsAttached: false,
+    dragging: false,
+    initX: NaN,
+    initY: NaN,
+    deltaX: 0,
+    deltaY: 0,
+    baseX: basePosition.x,
+    baseY: basePosition.y,
+  })
+
+  let clonedChildren = null
+
+  const move = useCallback(
+    (e: MouseEvent | TouchEvent) => {
+      const deltaX = getScreenDistance(e, 'screenX') - dragInfoRef.current.initX
+      const deltaY = getScreenDistance(e, 'screenY') - dragInfoRef.current.initY
+
+      dragInfoRef.current.deltaX = deltaX
+      dragInfoRef.current.deltaY = deltaY
+
+      // console.log({ deltaX, deltaY })
+
+      const { baseX, baseY } = dragInfoRef.current
+
+      onDrag?.({ x: baseX + deltaX, y: baseY + deltaY, deltaX, deltaY })
+    },
+    [onDrag]
+  )
+
+  const end = useCallback(() => {
+    window.removeEventListener('mousemove', move)
+    window.removeEventListener('mouseup', end)
+
+    window.removeEventListener('touchmove', move)
+    window.removeEventListener('touchend', end)
+
+    const { deltaX, deltaY, baseX, baseY } = dragInfoRef.current
+
+    onEnd?.({ x: baseX + deltaX, y: baseY + deltaY, deltaX, deltaY })
+  }, [move, onEnd])
+
+  const start = useCallback(
+    (e: MouseEvent | TouchEvent) => {
+      if (stopPropagation) {
+        e.stopPropagation()
+      }
+
+      e.preventDefault()
+
+      dragInfoRef.current.initX = getScreenDistance(e, 'screenX')
+      dragInfoRef.current.initY = getScreenDistance(e, 'screenY')
+
+      dragInfoRef.current.deltaX = 0
+      dragInfoRef.current.deltaY = 0
+
+      dragInfoRef.current.baseX = basePosition.x
+      dragInfoRef.current.baseY = basePosition.y
+
+      window.addEventListener('mousemove', move)
+      window.addEventListener('mouseup', end)
+
+      window.addEventListener('touchmove', move)
+      window.addEventListener('touchend', end)
+
+      onStart?.({ x: basePosition.x, y: basePosition.y, deltaX: 0, deltaY: 0 })
+
+      return () => {
+        window.removeEventListener('mousemove', move)
+        window.removeEventListener('mouseup', end)
+
+        window.removeEventListener('touchmove', move)
+        window.removeEventListener('touchend', end)
+      }
+    },
+    [move, end, stopPropagation, basePosition, onStart]
+  )
+
+  useEffect(() => {
+    const childElm = childRef.current
+
+    childElm.addEventListener('mousedown', start)
+    childElm.addEventListener('touchstart', start, { passive: false })
+
+    return () => {
+      childElm.removeEventListener('mousedown', start)
+      childElm.removeEventListener('touchstart', start)
+    }
+  }, [start])
+
+  try {
+    clonedChildren = map(only(children), (child) =>
+      React.cloneElement(child as React.ReactElement, {
+        ref: (ref: any) => (childRef.current = ref),
+      })
+    )
+  } catch (error) {
+    console.error('[Draggable] You should pass a single child component.')
+    return <>{children}</>
+  }
+
+  return <>{clonedChildren}</>
+}
+
+export default Draggable
